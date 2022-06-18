bed1c8761652f1c77de9a1615e589388e0c65bf8
John <jfkonecn@gmail.com>
2022-06-18 11:14:48 -0400

    setup main page layout

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -3,19 +3,27 @@ use yew::prelude::*;
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <>
-            <header class={classes!("bg-sky-100", "h-20")}>
-                <ul>
-                    <li>
-                        <a href="https://github.com/jfkonecn/scientists-toolbox">{"Github"}</a>
-                    </li>
-                </ul>
-            </header>
-            <main class={classes!("bg-white", "min-h-[calc(100vh-theme(spacing.20)-theme(spacing.20))]")}>
-            <p class={classes!("bg-red-100")}>{"Test!"}</p>
-            </main>
-            <footer class={classes!("bg-sky-100", "h-20")}>
-            </footer>
-       </>
+        <div class={classes!("flex", "items-center", "justify-center")}>
+            <div class={classes!("w-full", "lg:w-[theme(screens.lg)]")}>
+                <header class={classes!("bg-sky-100","flex", "items-center", "justify-center", "h-20", "p-10")}>
+                    <ul class={classes!("flex", "items-center", "h-full")}>
+                        <li>
+                            <a class={classes!("hover:underline")} href="">{"Scientist's Toolbox"}</a>
+                        </li>
+                    </ul>
+                    <div class={classes!("flex-grow")}></div>
+                    <ul class={classes!("flex", "items-center", "h-full")}>
+                        <li>
+                            <a class={classes!("hover:underline")} href="https://github.com/jfkonecn/scientists-toolbox">{"Github"}</a>
+                        </li>
+                    </ul>
+                </header>
+                <main class={classes!("bg-white", "min-h-[calc(100vh-theme(spacing.20)-theme(spacing.20))]")}>
+                    <p class={classes!("bg-red-100")}>{"Test!"}</p>
+                </main>
+                <footer class={classes!("bg-sky-100", "h-20")}>
+                </footer>
+            </div>
+        </div>
     }
 }

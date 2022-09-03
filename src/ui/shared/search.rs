2bb3fe000555f4506cb0a6495b995366a4394900
John <jfkonecn@gmail.com>
2022-09-03 14:28:16 -0400

    search now works

--- src/ui/shared/search.rs
+++ src/ui/shared/search.rs
@@ -1,12 +1,15 @@
+use crate::thermo::steam::SatQuery;
 use crate::ui::app::{MainRoute, ThermoRoute};
 use crate::ui::assets::svg::*;
 use strum::IntoEnumIterator;
 use wasm_bindgen::JsCast;
 use web_sys::{EventTarget, HtmlInputElement, InputEvent};
 use yew::{classes, function_component, html, use_state, Callback, Properties};
+use yew_router::prelude::*;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum AllRoutes {
+    MainRoute(MainRoute),
     ThermoRoute(ThermoRoute),
 }
 
@@ -29,11 +32,29 @@ fn create_link_configs() -> Vec<SearchableGroup> {
         tags: vec![],
         label: "Main".to_owned(),
         configs: MainRoute::iter()
-            .map(|route| match route {
-                MainRoute::Home
-                | MainRoute::NotFound
-                | MainRoute::Thermo
-                | MainRoute::ThermoRoot => None,
+            .map(|route| {
+                let opt: Option<(String, Vec<String>)> = match route {
+                    // ThermoRoute::SteamTable => Some(("Steam Table".to_owned(), vec![])),
+                    MainRoute::Home => Some(("Home".to_owned(), vec!["test".to_owned()])),
+                    MainRoute::NotFound => Some((
+                        "Not Found".to_owned(),
+                        vec!["something".to_owned(), "test".to_owned()],
+                    )),
+                    MainRoute::Thermo | MainRoute::ThermoRoot => None,
+                    // MainRoute::Home
+                    // | MainRoute::NotFound
+                    // | MainRoute::Thermo
+                    // | MainRoute::ThermoRoot => None,
+                };
+                if let Some((label, tags)) = opt {
+                    Some(SearchableLinkConfig {
+                        tags,
+                        label,
+                        route: AllRoutes::MainRoute(route),
+                    })
+                } else {
+                    None
+                }
             })
             .flatten()
             .collect::<Vec<SearchableLinkConfig>>(),
@@ -68,6 +89,33 @@ fn create_link_configs() -> Vec<SearchableGroup> {
 }
 
 #[derive(Properties, PartialEq)]
+struct SearchResultLinkProps<T: Routable> {
+    to: T,
+    label: String,
+}
+
+#[function_component(SearchResultLink)]
+fn search_link_result<T: Routable + 'static>(
+    SearchResultLinkProps { to, label }: &SearchResultLinkProps<T>,
+) -> html {
+    let to = to.clone();
+    html! {
+    <Link<T>
+        classes={classes!(
+            "w-full", "h-full", "block",
+            "justify-center", "content-center",
+            "flex", "flex-col"
+        )}
+        to={to.clone()}
+        >
+        <div class={classes!("p-3")}>
+            {label}
+        </div>
+    </Link<T>>
+    }
+}
+
+#[derive(Properties, PartialEq)]
 struct SearchResultProps {
     group: SearchableGroup,
 }
@@ -75,14 +123,43 @@ struct SearchResultProps {
 #[function_component(SearchResult)]
 fn search_result(SearchResultProps { group }: &SearchResultProps) -> html {
     html! {
-        <output>
-            <h2>{group.label.to_owned()}</h2>
-            <ul>
+        <output class={classes!("flex", "flex-col", "gap-4")}>
+            <h2
+            class={classes!("font-bold")}
+            >
+                {group.label.to_owned()}
+            </h2>
+            <ul class={classes!("flex", "flex-col", "gap-1")}>
             {
-                for group.configs.iter().map(|x| {
+                for group.configs.iter().map(|config| {
+                    let link = match config.route {
+                        AllRoutes::MainRoute(route) => {
+                            html! {
+                            <SearchResultLink<MainRoute>
+                                to={route}
+                                label={config.label.to_owned()}
+                                />
+
+                            }
+                        }
+                        AllRoutes::ThermoRoute(route) => {
+                            html! {
+                            <SearchResultLink<ThermoRoute>
+                                to={route}
+                                label={config.label.to_owned()}
+                                />
+
+                            }
+                        }
+                    };
                     html! {
-                        <li>
-                            {x.label.to_owned()}
+
+                        <li class={classes!(
+                            "w-full", "h-16",
+                            "rounded-lg",
+                            "bg-gray-200", "hover:bg-sky-100",
+                        )}>
+                            {link}
                         </li>
                     }
                 })
@@ -111,9 +188,58 @@ pub fn search() -> Html {
             None
         }
     });
-    let configs = create_link_configs();
+    let configs = {
+        let search_opt = (*search_opt).clone().and_then(|x| {
+            if x.len() > 0 {
+                Some(x.to_lowercase())
+            } else {
+                None
+            }
+        });
+        let configs = create_link_configs();
+        if let Some(keyword) = search_opt {
+            configs
+                .iter()
+                .map(|group| {
+                    if group.label.to_lowercase().contains(&keyword)
+                        || group
+                            .tags
+                            .iter()
+                            .any(|x| x.to_lowercase().contains(&keyword))
+                    {
+                        Some(group.clone())
+                    } else {
+                        let sub_configs = group
+                            .configs
+                            .iter()
+                            .filter(|config| {
+                                config.label.to_lowercase().contains(&keyword)
+                                    || config
+                                        .tags
+                                        .iter()
+                                        .any(|x| x.to_lowercase().contains(&keyword))
+                            })
+                            .map(|x| x.clone())
+                            .collect::<Vec<SearchableLinkConfig>>();
+                        if sub_configs.len() > 0 {
+                            Some(SearchableGroup {
+                                label: group.label.clone(),
+                                configs: sub_configs,
+                                tags: group.tags.clone(),
+                            })
+                        } else {
+                            None
+                        }
+                    }
+                })
+                .flatten()
+                .collect::<Vec<SearchableGroup>>()
+        } else {
+            configs
+        }
+    };
     html! {
-        <form class={classes!("grid", "w-full", "content-start")}>
+        <form class={classes!("flex", "flex-col", "gap-3")}>
             <fieldset class={classes!("h-10", "relative")}>
                 <div
                 class={classes!("grid", "h-full","absolute",
@@ -126,23 +252,14 @@ pub fn search() -> Html {
                 <input
                 class={classes!("inline-block", "pl-10",
                  "pr-3", "py-2", "w-full", "h-full",
-                 "rounded-full", "shadow-md",
-                "border-2")}
+                 "border-2", "rounded-md", "border-gray-200")}
                     type={"text"}
                     placeholder={"Search..."}
                     oninput={oninput}
                 />
             </fieldset>
-            <div>{
-                if let Some(ref val) = *search_opt {
-                    val.clone()
-                } else {
-                    "".to_string()
-                }
-            }</div>
             {
-                for
-                configs.iter().map(|x| {
+                for configs.iter().map(|x| {
                     html! {
                         <SearchResult group={x.clone()} />
                     }

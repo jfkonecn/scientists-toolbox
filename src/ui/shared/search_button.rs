68e1183fb6b917e81b7d716b1419d473ea6387f7
John <jfkonecn@gmail.com>
2022-11-12 10:00:55 -0500

    removed search modal

--- src/ui/shared/search_button.rs
+++ src/ui/shared/search_button.rs
@@ -1,72 +1,15 @@
-use super::modal::*;
-use super::search::*;
 use crate::ui::app::MainRoute;
 use crate::ui::assets::svg::*;
-use web_sys::MouseEvent;
 use yew::prelude::*;
-use yew::{function_component, use_state_eq, Callback};
-use yew_router::prelude::use_route;
+use yew_router::prelude::*;
 
 #[function_component(SearchButton)]
 pub fn search_button() -> Html {
-    let location = use_route::<MainRoute>();
-    let show_modal = use_state_eq(|| false);
-    let toggle_show_modal = {
-        let show_modal = show_modal.clone();
-        let show_modal_value = !*show_modal;
-        Callback::from(move |_: MouseEvent| {
-            show_modal.set(show_modal_value);
-        })
-    };
-
-    let close_modal = {
-        let show_modal = show_modal.clone();
-        Callback::from(move |_: Event| {
-            show_modal.set(false);
-        })
-    };
-
-    {
-        let location = location.clone();
-        let show_modal = show_modal.clone();
-        use_effect_with_deps(
-            move |_| {
-                show_modal.set(false);
-                || {}
-            },
-            location,
-        );
-    }
-
-    let on_submit = Callback::from(move |e: FocusEvent| {
-        e.prevent_default();
-    });
-
     html! {
-        <>
-            <button
-                class={classes!("w-full", "h-full")}
-                onclick={toggle_show_modal}>
-                <MagnifyingGlass/>
-            </button>
-            {
-                if *show_modal {
-                    html! {
-                    <Modal
-                        title={"Search".to_owned()}
-                        on_close_requested={close_modal}
-                        on_submit={on_submit}
-                        >
-                        <Search/>
-                    </Modal>
-
-                    }
-                } else {
-                    html! {
-                        <></>
-                    }
-                }
-            }
-        </>
+    <Link<MainRoute>
+        classes={classes!("w-full", "h-full")}
+        to={MainRoute::Home}>
+        <MagnifyingGlass/>
+    </Link<MainRoute>>
     }
 }
